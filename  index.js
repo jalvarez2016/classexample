8aff464b91eba72121f2d3f33fd4e2196b0c16cc
Jason Alvarez <jalvarez@squarespace.com>
2023-10-26 15:21:01 -0400

    first commit

---
+++  index.js
@@ -0,0 +1,13 @@
+console.log("frog")
+const animal = "frog";
+const number = 14;
+const bool = false
+const frogs = {
+  amount: 3,
+  names: [
+    'feeb',
+    'feebette',
+    'MacIntosh'
+  ]
+}
+console.log(frogs.search)
