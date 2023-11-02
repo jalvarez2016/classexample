713a2a27ea195f955cd63cfb84c535fc4c6df07d
Jason Alvarez <jalvarez@squarespace.com>
2023-11-02 15:20:35 -0400

    Revert "Randy theclock"

--- frontpage.js
+++ frontpage.js
@@ -3,10 +3,3 @@ console.log('frontpage')
 console.log('end')
 
 console.log('check protection rules')
-
-console.log("check protection rules. V2");
-
-// git add
-// git status
-// git commit -m
-// git push
